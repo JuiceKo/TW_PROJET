14220d25a4ab3dbd8fb8238c18e34a7fd2e0ebf4
Elise BEAUVY <94043023+Elise-Beauvy@users.noreply.github.com>
2025-01-11 18:54:18 +0100

    login

--- DBE_project/chat_app/views.py
+++ DBE_project/chat_app/views.py
@@ -52,3 +52,21 @@ def create_chatroom(request):
             Channel.objects.create(name=name)
         return redirect('chatroom_list')
     return render(request, 'chat_app/create_chatroom.html')
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Connexion réussie!')
+            return redirect('chatroom_list')
+        else:
+            messages.error(request, 'Nom d\'utilisateur ou mot de passe incorrect.')
+
+    return render(request, 'chat_app/login.html')
